93d4a2468e9b632f79e6d9df3e2c15bb8b55a43e
ergav <72596720+ergav@users.noreply.github.com>
2023-02-05 11:35:38 +0100

    laser reverted

--- Assets/Scripts/Invaders/LaserBeam.cs
+++ Assets/Scripts/Invaders/LaserBeam.cs
@@ -15,7 +15,7 @@ public class LaserBeam : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag != "Enemy")
         {
             Destroy(gameObject);
         }
